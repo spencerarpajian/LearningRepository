26e2c17bc26a91cf095d15466faad751b0dc6530
Spencer Arpajian <spence@Spencers-MacBook-Pro.local>
2024-12-05 15:29:01 +0000

    Changed theme and labels on BMI histogram

--- PracticeFile.R
+++ PracticeFile.R
@@ -18,5 +18,6 @@ health_data <- NHANES %>% select(BMI,Age,Gender)
 #Histogram from object data
 
 ggplot(health_data, aes(x = BMI))+
-geom_histogram(binwidth = 1)
-
+geom_histogram(binwidth = 1, fill = "skyblue", color = "black") +
+labs(title = "Mean Weight in Wales is Higher Than European Average") +
+theme_dark()
